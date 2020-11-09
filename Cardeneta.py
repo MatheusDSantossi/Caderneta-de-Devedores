f365fe8b986044215b0f1078f9dc15968ac8c280
Matheus Santos <67028798+MatheusS1819@users.noreply.github.com>
2020-11-09 16:38:15 -0300

    Programa "completo".
    Quero terminar o programa com uma soma da conta_velha com a conta_nova, mas, não estou conseguindo devido a alguns erros com variáveis no Entry. Quando consegui resolver atualizo.

--- Cardeneta.py
+++ Cardeneta.py
@@ -16,15 +16,15 @@ class Pessoas:
         self.wind.title("Caderneta de Clientes")
         self.wind.configure(bg="black")
 
-        frame = LabelFrame(self.wind, text = "Cadastrar Novo Cliente na Caderneta: ", bg="white", font=("Times New Roman", 20), padx= 10, pady=145)
+        frame = LabelFrame(self.wind, text = "Cadastrar Novo Cliente na Caderneta: ", bg="lightblue", fg="darkgreen",font=("Times New Roman", 20), padx= 10, pady=140)
         frame.grid(row=0, column=0, columnspan=10, pady=0)
 
-        nomeDoC = Label(frame, text="Nome do Cliente: ", bg="white", font=("Times", 15)).grid(row=1, column=0)
+        nomeDoC = Label(frame, text="Nome do Cliente: ", bg="lightblue", fg="darkgreen", font=("Times", 15)).grid(row=1, column=0)
         
         self.cliente = Entry(frame, font=10)
         self.cliente.grid(row=1, column=1)
 
-        valorNaC = Label(frame, text="Valor Na Conta: ", bg="white", font=("Times", 15)).grid(row=2, column=0)
+        valorNaC = Label(frame, text="Valor Na Conta: ", bg="lightblue", fg="darkgreen", font=("Times", 15)).grid(row=2, column=0)
         
         self.conta = Entry(frame, font=10)
         self.conta.grid(row=2, column=1)
@@ -32,7 +32,7 @@ class Pessoas:
         adicionar = ttk.Button(frame, text="Novo Cliente", command=self.adding_customer)
         adicionar.grid(row=3, columnspan=2, sticky=W+E)
 
-        self.message = Label(frame, text="", bg="white", fg="red")
+        self.message = Label(frame, text="", bg="lightblue", fg="red")
         self.message.grid(row=8, column=0)
 
         self.tree = ttk.Treeview(height=21, column=2)
@@ -42,6 +42,10 @@ class Pessoas:
 
         deletar = ttk.Button(frame, text="Deletar Cliente", command=self.deleting_customer)
         deletar.grid(row=4, columnspan=2, sticky=W+E)
+
+        editar = ttk.Button(frame, text="Editar", command=self.editing_costumer)
+        editar.grid(row=5, columnspan=2, sticky=W+E)
+
 
         self.viewing_records()
 
@@ -118,7 +122,7 @@ class Pessoas:
         
         self.viewing_records()
 
-    def editing_costumer(self, new_costumer, old_conta):
+    def editing_costumer(self):
 
         self.message["text"] = ""
 
@@ -132,7 +136,7 @@ class Pessoas:
             return
         
         cliente = self.tree.item(self.tree.selection())["text"]
-        old_check = self.tree.item(self.tree.selection())["values"][0]
+        conta_velha = self.tree.item(self.tree.selection())["values"][0]
 
         self.edit_wind = Toplevel()
         self.edit_wind.title("titulo : EDITAR PRODUTO: ")
@@ -147,7 +151,7 @@ class Pessoas:
         y = (screen_height / 2) - (height / 2)
 
         self.edit_wind.geometry("%dx%d+%d+%d" % (width, height, x, y)) 
-        self.edit_wind.resizable(0, 0)
+        # self.edit_wind.resizable(0, 0)
 
         self.edit_wind.configure(bg = "DarkBlue")
 
@@ -155,9 +159,35 @@ class Pessoas:
         Entry(self.edit_wind, textvariable=StringVar(self.edit_wind, value=cliente), state="readonly").grid(row=0, column=2)
         Label(self.edit_wind, text=" DIGITE O NOVO CLIENTE: ", bg="DarkBlue", fg="WHITE").grid(row=1, column=1)
 
+        novo_cliente = Entry(self.edit_wind)
+        novo_cliente.grid(row=1, column=2)
+
+        Label(self.edit_wind, text=f"CONTA ANTIGA DO(a) {cliente}: ", bg="WHITE").grid(row=2, column=1)
+        Entry(self.edit_wind, textvariable=DoubleVar(self.edit_wind, value=conta_velha), state="readonly").grid(row=2, column=2)
+        Label(self.edit_wind, text=f" DIGITE A NOVA CONTA DO(a) {conta_velha}", bg="DarkBlue", fg="WHITE").grid(row=3, column=1)
+
+        conta_nova = Entry(self.edit_wind)
+        Label(self.edit_wind, text=f"a {conta_nova.get()}", bg="WHITE").grid(row=5, column=1)
+        # if conta_nova:
+        #     texto = conta_nova.get()
+        #     numero = str(texto)
+        #     conta_somada = numero + str(conta_velha)
+        
+        conta_nova.grid(row=3, column=2)
+
+        button = Button(self.edit_wind, bd=2, text=f"EDITAR CLIENTE ", bg="black", fg="yellow", command=lambda:self.edit_records(novo_cliente.get(), conta_nova.get(), cliente, conta_velha)).grid(row=5, column=1, sticky=W)
+        self.edit_wind.mainloop()
 
 
+    def edit_records(self, cliente, conta_nova, cliente_velho, conta_velha):
 
+        query = "UPDATE clientes SET cliente=?, conta=? WHERE cliente=? AND conta=?"
+        paramenters = (cliente, conta_nova, cliente_velho, conta_velha)
+
+        self.run_query(query, paramenters)
+        self.edit_wind.destroy()
+        self.message["text"] = f"CADASTRO {cliente_velho} EDITADO COM SUCESSO!"
+        self.viewing_records()
             
     
 if __name__ == "__main__":
